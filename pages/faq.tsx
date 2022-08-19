526578baf0968972ba91425c95a1b764e2e4dce2
Gabriel Lenon <glenonsilva@gmail.com>
2022-08-19 09:14:40 -0300

    feat: :tada: Primeiro commit do projeto
    Primeiro commit do projeto

---
+++ pages/faq.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+import type { NextPage } from 'next';
+
+const Faq: NextPage = () => {
+  return (
+    <div>
+      <h1>Alura Cases - Página de Perguntas FAQ</h1>
+      <a href="/">Ir para a Home</a>
+    </div>
+  )
+};
+
+export default Faq;
