3fc4fbc1058ad379ff5b175d0397b309c58c8bcf
Gabriel Lenon <glenonsilva@gmail.com>
2022-08-25 00:27:01 -0300

    :rocket: Avança com Script

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script';
 import { ReactNode } from 'react';
 
 interface LayoutProps {
@@ -8,6 +9,21 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <>
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=328704946"></Script>
+      <Script id='myScript' dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+    
+              gtag('config', '328704946');
+            `
+        }}
+      />
+
+      {/* </Script> */}
+
       <Head>
         <title>Meu App Next.js</title>
       </Head>
